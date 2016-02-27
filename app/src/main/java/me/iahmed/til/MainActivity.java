30880ea2c5e5a463656af6953fae306fd99bb5c7
hazrmard <ibrahim78786@gmail.com>
2016-02-27 16:20:51 -0600

    Learned basic UI. Created ReadRedditTitle class.

--- app/src/main/java/me/iahmed/til/MainActivity.java
+++ app/src/main/java/me/iahmed/til/MainActivity.java
@@ -3,31 +3,41 @@ package me.iahmed.til;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.HapticFeedbackConstants;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+import me.iahmed.til.ReadRedditTitle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        int SDK = android.os.Build.VERSION.SDK_INT;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         TextView main_text = (TextView) findViewById(R.id.main_text);
-        main_text.setText("HELLO SHITS!");
+        main_text.setText(R.string.greeting);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.fab_circle));
+        fab.setHapticFeedbackEnabled(true);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "MA BRO!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                //Snackbar.make(view, "MA BRO!", Snackbar.LENGTH_LONG)
+                //  .setAction("Action", null).show();
             }
         });
     }
@@ -49,7 +59,16 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.night_mode) {
+            item.setChecked(!item.isChecked());
+            if (item.isChecked()) {
+                Toast.makeText(this, R.string.night_mode_en, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, R.string.night_mode_den, Toast.LENGTH_SHORT).show();
+            }
+            return true;
         }
+
 
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/me/iahmed/til/ReadRedditTitle.java
@@ -0,0 +1,11 @@
+package me.iahmed.til;
+
+/**
+ * Created by Ibrahim on 2/27/2016.
+ */
+public class ReadRedditTitle {
+
+    static void authenticate(){
+        System.out.println("Authenticating.....");
+    }
+}
