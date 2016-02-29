305b6bea6309c435526fcce3eb390498242dc108
hazrmard <ibrahim78786@gmail.com>
2016-02-28 22:20:21 -0600

    Added about link. Custom launcher icon.

--- app/src/main/java/me/iahmed/til/MainActivity.java
+++ app/src/main/java/me/iahmed/til/MainActivity.java
@@ -98,7 +98,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.about) {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://github.com/hazrmard/TIL"));
+            startActivity(browserIntent);
             return true;
         } else if (id == R.id.night_mode) {
             item.setChecked(!item.isChecked());
